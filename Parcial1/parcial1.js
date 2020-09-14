298a8a43090aac31077bedb80ec0c90af6de70b2
LGsus Rmz <caednicolas2@gmail.com>
2020-09-14 15:23:03 -0500

    EditCorrecciondeREQUERIMENTOS

--- Parcial1/parcial1.js
+++ Parcial1/parcial1.js
@@ -8,14 +8,14 @@ L.tileLayer(tilesP[0], {
     subdomains: 'abcd',
     accessToken: "pk.eyJ1IjoiemVza3kiLCJhIjoiY2p6b2k4ejJ3MDB1ZjNjcGR5aWFjcXI4YyJ9.3tK4ewbOKRAEw9BDA4xAkQ", // Map Box Default public token
 }).addTo(MapView); // Creates the map
-ChangeMap(1);
+//ChangeMap(1);
 var newIcon = L.icon({ // Crea un template para los Iconos.
     iconUrl:'../ubicacion.png',
     iconSize:[60,60],
     iconAnchor:[30,60],
     popupAnchor:[-3,-76]
 });
-
+document.getElementById("Mapviewres").style.display = "none";
 var Global = [];
 MapView.doubleClickZoom.disable(); // Deshabilitar el zoom en doble click;
 MapView.on('dblclick', function(e){ 
@@ -24,13 +24,16 @@ MapView.on('dblclick', function(e){
     Global.push(latlng);
     //console.log(Global);
     ClearMap();
-    if (Global.length >= 10) {
+    if (Global.length > 10) {
+        ClearMapRes();
+        document.getElementById("Mapviewres").style.display = "block";
         var nums = Math.floor(Math.random() * 3);
         console.log(nums);
         ChangeMap(nums);
-        var Polygon = L.polygon([Global]).addTo(MapViewres);
+        var Polygon = L.polygon([Global]).addTo(MapView);
         var prints= "";
         for (let i = 0; i < Global.length; i++) {
+             L.marker([Global[i].lat, Global[i].lng],{icon:newIcon}).addTo(MapViewres);
              prints += 'Punto numero '+(i+1)+': '+ Global[i].lat +','+ Global[i].lng + '\n';
         }
 
@@ -39,6 +42,14 @@ MapView.on('dblclick', function(e){
 });
 
 function ClearMap() {
+    for(MapViewLayers in MapView._layers) {
+        if(MapView._layers[MapViewLayers]._path != undefined) {
+            MapView.removeLayer(MapView._layers[MapViewLayers]);
+        }
+    }
+}
+
+function ClearMapRes() {
     for(MapViewLayers in MapViewres._layers) {
         if(MapViewres._layers[MapViewLayers]._path != undefined) {
             MapViewres.removeLayer(MapViewres._layers[MapViewLayers]);
